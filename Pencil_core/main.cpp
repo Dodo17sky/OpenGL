cc717e935f53d8a2b8b91dd240262422ae05330a
Dorin work <dodoyln@hotmail.com>
2018-05-24 16:05:42 +0300

    First window

--- Pencil_core/main.cpp
+++ Pencil_core/main.cpp
@@ -1,8 +1,42 @@
+#include <iostream>
+
 #include "glad\glad.h"
 #include "GLFW\glfw3.h"
 
+void resize_callback(GLFWwindow* win, int width, int height);
+
 int main()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(480, 360, "Pencil game engine", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initilize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 480, 360);
+    glfwSetFramebufferSizeCallback(window, resize_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
 
     return 0;
+}
+
+void resize_callback(GLFWwindow* win, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }

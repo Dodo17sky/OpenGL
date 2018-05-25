a97b124384713ed7b52d3f1c190f03ece335198b
Dorin work <dodoyln@hotmail.com>
2018-05-25 14:15:05 +0300

    Element buffer object

--- Pencil_core/main.cpp
+++ Pencil_core/main.cpp
@@ -11,13 +11,12 @@ void resize_callback(GLFWwindow* win, int width, int height);
 // static functions
 void processInput(GLFWwindow* win);
 
+// Global variables
+GLenum polygonMode = GL_FILL;
+
 int main()
 {
     glfwInit();
-        // Core OpenGL requires that we use a VAO so it knows what to do with our vertex inputs. 
-        // In OpenGL 3.3 is not generated any default vertex array
-        // Newer version will auto generate a default vertex array
-        // If we fail to bind a VAO, OpenGL will most likely refuse to draw anything.
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -34,37 +33,61 @@ int main()
         std::cout << "Failed to initilize GLAD" << std::endl;
         return -1;
     }
-    std::cout << glGetString(GL_VERSION) << std::endl;
+    else {
+        std::cout << glGetString(GL_VERSION) << std::endl;
+    }
+
     glClearColor(0.2, 0.5, 0.7, 1.0);
     glViewport(0, 0, 480, 360);
     glfwSetFramebufferSizeCallback(window, resize_callback);
 
     Pencil::Shader shader("src/graphics/shaders/basic.vertex", "src/graphics/shaders/basic.fragment");
+    shader.disable();
 
     //Define triangle coordinates
     float coords[] = {
-        -0.7, 0.0, 0.0,
-         0.7, 0.0, 0.0,
-         0.0, 1.0, 0.0
+        -0.7, -0.7, 0.0,
+        -0.7,  0.7, 0.0,
+         0.7,  0.7, 0.0,
+         0.7, -0.7, 0.0,
     };
-    GLuint vbId, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &vbId);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, vbId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0); 
+    GLuint indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
 
-    shader.enable();
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    // Step 1: Bind Vertex-Array
+    glBindVertexArray(VAO);
+    
+    // Step 2: Send array of vertices to gpu - all positions
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STATIC_DRAW);
+
+    // Step 3: Send array of indices to gpu - what positions to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Step 4: set "Vertex attribute"
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         processInput(window);
 
-        // OpenGL version 3.3 - we have a VAO binded before
-        glDrawArrays(GL_TRIANGLES, 0, 3); 
+        glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -83,4 +106,7 @@ void processInput(GLFWwindow * win)
 {
     if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(win, 1);
+
+    if(glfwGetKey(win, GLFW_KEY_P) == GLFW_PRESS)
+        polygonMode = ((polygonMode==GL_FILL)?GL_LINE:GL_FILL);
 }
